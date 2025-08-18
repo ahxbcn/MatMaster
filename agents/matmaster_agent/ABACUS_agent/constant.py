17d7d1c57607410cdfb532b4d604c9836df6dc8a
ahxbcn <2092233074@qq.com>
2025-08-18 16:10:46 +0800

    Update ABACUS test MCP server

--- agents/matmaster_agent/ABACUS_agent/constant.py
+++ agents/matmaster_agent/ABACUS_agent/constant.py
@@ -3,13 +3,14 @@ import copy
 from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge, LOCAL_EXECUTOR, CURRENT_ENV
 
 if CURRENT_ENV in ["test", "uat"]:
-    ABACUS_CALCULATOR_URL = 'http://tfhs1357255.bohrium.tech:50001/sse'
+    ABACUS_CALCULATOR_URL = 'http://xaoi1371867.bohrium.tech:50001/sse'
 else:
+    #ABACUS_CALCULATOR_URL = 'http://xaoi1371867.bohrium.tech:50001/sse'
     ABACUS_CALCULATOR_URL = 'https://abacus-agent-tools-uuid1751014104.app-space.dplink.cc/sse?token=7cae849e8a324f2892225e070443c45b'
 ABACUS_AGENT_NAME = "ABACUS_calculation_agent"
 ABACUS_CALCULATOR_BOHRIUM_EXECUTOR = copy.deepcopy(BohriumExecutor)
 ABACUS_CALCULATOR_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["image_address"] = \
-    "registry.dp.tech/dptech/dp/native/prod-22618/abacusagenttools:v0.1-20250813"
+    "registry.dp.tech/dptech/dp/native/prod-22618/abacusagenttools:v0.1-20250807-2"
 ABACUS_CALCULATOR_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
 
 EXECUTOR_MAP = {
